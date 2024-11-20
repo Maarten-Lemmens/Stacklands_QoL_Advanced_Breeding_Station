84d679c3a7182a28d6e9a4462af4390b9c2af0fa
bureauPlant <bureau_plant@mail.com>
2024-11-20 23:41:39 +0100

    v0.0.1: Advanced Breeding Station
    A high-tech building for breeding animals. Automatically keeps the original animals.

--- Mod.cs
+++ Mod.cs
@@ -3,13 +3,46 @@ using System;
 using System.Collections;
 using UnityEngine;
 
-namespace ExampleModNS
+namespace AdvancedBreedingStationModNS
 {
-    public class ExampleMod : Mod
+    public class AdvancedBreedingStationMod : Mod
     {
         public override void Ready()
         {
-            Logger.Log("Ready!");
+            WorldManager.instance.GameDataLoader.AddCardToSetCardBag(SetCardBagType.AdvancedBuildingIdea, "blueprint_advanced_breeding_station", 1);
         }
     }
+
+    public class AdvancedBreedingStation : BreedingPen
+	{
+		// Overwrite required to put timer to 60 seconds (instead of the default hard-coded 120 seconds). 
+        public override void UpdateCard()
+        {
+            if (MyGameCard.GetChildCount() == 2)
+            {
+                MyGameCard.StartTimer(60f, BreedAnimals, SokLoc.Translate("card_advanced_breeding_station_status"), GetActionId("BreedAnimals"));
+            }
+            else if (MyGameCard.GetChildCount() > 2)
+            {
+                GameCard gameCard = MyGameCard.TryGetNthChild(3);
+                if (gameCard != null)
+                {
+                    gameCard.RemoveFromParent();
+                }
+                MyGameCard.CancelTimer(GetActionId("BreedAnimals"));
+            }
+            else
+            {
+                MyGameCard.CancelTimer(GetActionId("BreedAnimals"));
+            }
+        }
+        
+        [TimedAction("breed_animals")]
+        public void BreedAnimals()
+        {
+            CardData cardData = WorldManager.instance.CreateCard(base.transform.position, MyGameCard.Child.CardData.Id);
+            WorldManager.instance.StackSendCheckTarget(MyGameCard, cardData.MyGameCard, OutputDir);
+            QuestManager.instance.SpecialActionComplete("breed_" + cardData.Id);        
+        }
+	}
 }
